1dca71c86b7cedc668e6eb705087d55cbbb413d1
HwangBBang <h1009218@hufs.ac.kr>
2024-02-25 17:34:35 +0900

    ERD design, export to sql
    Co-Authored-By: 김지섭 <0226daniel@gmail.com>

---
+++ Hufs Reservation.sql
@@ -0,0 +1,127 @@
+﻿CREATE TABLE `Room` (
+	`roomId`	BIGINT	NOT NULL,
+	`timetableConfigId`	BIGINT	NOT NULL,
+	`name`	VARCHAR(255)	NULL
+);
+
+CREATE TABLE `Partition` (
+	`partitionId`	BIGINT	NOT NULL,
+	`timetableConfigId`	BIGINT	NOT NULL,
+	`roomId`	BIGINT	NOT NULL
+);
+
+CREATE TABLE `Reservation` (
+	`reservationId`	BIGINT	NOT NULL,
+	`userId`	BIGINT	NOT NULL,
+	`partitionId`	BIGINT	NOT NULL,
+	`timetableId`	BIGINT	NOT NULL,
+	`roomId`	BIGINT	NOT NULL,
+	`timetableIndexFrom`	INT	NULL,
+	`timetableIndexTo`	INT	NULL,
+	`state`	ENUM (  'RESERVATED', 'VISITED', 'NOSHOW' )	NULL
+);
+
+CREATE TABLE `User` (
+	`userId`	BIGINT	NOT NULL,
+	`serial`	INT	NULL,
+	`name`	VARCHAR(30)	NULL,
+	`password`	VARCHAR(255)	NULL,
+	`isAdmin`	TYNYINT(1)	NULL
+);
+
+CREATE TABLE `TimetableConfig` (
+	`timetableConfigId`	BIGINT	NOT NULL,
+	`startTime`	DATETIME	NULL,
+	`endTime`	DATETIME	NULL,
+	`intervalMinute`	INT	NULL,
+	`maxSlots`	DATE	NULL
+);
+
+CREATE TABLE `Untitled4` (
+	`timetableId`	BIGINT	NOT NULL,
+	`startTime`	DATETIME	NULL,
+	`endTime`	DATETIME	NULL,
+	`intervalMinute`	INT	NULL,
+	`maxSlots`	DATE	NULL
+);
+
+ALTER TABLE `Room` ADD CONSTRAINT `PK_ROOM` PRIMARY KEY (
+	`roomId`,
+	`timetableConfigId`
+);
+
+ALTER TABLE `Partition` ADD CONSTRAINT `PK_PARTITION` PRIMARY KEY (
+	`partitionId`,
+	`timetableConfigId`,
+	`roomId`
+);
+
+ALTER TABLE `Reservation` ADD CONSTRAINT `PK_RESERVATION` PRIMARY KEY (
+	`reservationId`,
+	`userId`,
+	`partitionId`,
+	`timetableId`,
+	`roomId`
+);
+
+ALTER TABLE `User` ADD CONSTRAINT `PK_USER` PRIMARY KEY (
+	`userId`
+);
+
+ALTER TABLE `TimetableConfig` ADD CONSTRAINT `PK_TIMETABLECONFIG` PRIMARY KEY (
+	`timetableConfigId`
+);
+
+ALTER TABLE `Untitled4` ADD CONSTRAINT `PK_UNTITLED4` PRIMARY KEY (
+	`timetableId`
+);
+
+ALTER TABLE `Room` ADD CONSTRAINT `FK_TimetableConfig_TO_Room_1` FOREIGN KEY (
+	`timetableConfigId`
+)
+REFERENCES `TimetableConfig` (
+	`timetableConfigId`
+);
+
+ALTER TABLE `Partition` ADD CONSTRAINT `FK_Room_TO_Partition_1` FOREIGN KEY (
+	`timetableConfigId`
+)
+REFERENCES `Room` (
+	`timetableConfigId`
+);
+
+ALTER TABLE `Partition` ADD CONSTRAINT `FK_Room_TO_Partition_2` FOREIGN KEY (
+	`roomId`
+)
+REFERENCES `Room` (
+	`roomId`
+);
+
+ALTER TABLE `Reservation` ADD CONSTRAINT `FK_User_TO_Reservation_1` FOREIGN KEY (
+	`userId`
+)
+REFERENCES `User` (
+	`userId`
+);
+
+ALTER TABLE `Reservation` ADD CONSTRAINT `FK_Partition_TO_Reservation_1` FOREIGN KEY (
+	`partitionId`
+)
+REFERENCES `Partition` (
+	`partitionId`
+);
+
+ALTER TABLE `Reservation` ADD CONSTRAINT `FK_Partition_TO_Reservation_2` FOREIGN KEY (
+	`roomId`
+)
+REFERENCES `Partition` (
+	`roomId`
+);
+
+ALTER TABLE `Reservation` ADD CONSTRAINT `FK_Untitled4_TO_Reservation_1` FOREIGN KEY (
+	`timetableId`
+)
+REFERENCES `Untitled4` (
+	`timetableId`
+);
+
