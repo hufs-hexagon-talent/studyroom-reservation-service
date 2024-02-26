7602295fd21dc1e4b3b63760c027a6b9320faa12
HwangBBang <h1009218@hufs.ac.kr>
2024-02-26 20:47:36 +0900

    fixed test DTO

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/repository/ReservationUpdateDto.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/repository/ReservationUpdateDto.java
@@ -6,10 +6,18 @@ import lombok.Data;
 @Data
 public class ReservationUpdateDto {
     private Enum<State> state;
-
+    private Long roomId;
+    private Long partitionId;
+    private Long reservationId;
     public ReservationUpdateDto() {
     }
     public ReservationUpdateDto(Enum<State> state) {
+        this.state = state;
+    }
+
+    public ReservationUpdateDto(Long roomId, Long partitionId, Enum<State> state){
+        this.roomId = roomId;
+        this.partitionId = partitionId;
         this.state = state;
     }
 
