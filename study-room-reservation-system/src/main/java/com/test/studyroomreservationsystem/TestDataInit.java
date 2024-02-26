92fbd965d95cff0e87636f9dcf97032b977f6774
HwangBBang <h1009218@hufs.ac.kr>
2024-02-26 17:13:31 +0900

    add testData

---
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/TestDataInit.java
@@ -0,0 +1,26 @@
+package com.test.studyroomreservationsystem;
+
+import com.test.studyroomreservationsystem.domain.Reservation;
+import com.test.studyroomreservationsystem.domain.State;
+import com.test.studyroomreservationsystem.repository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@Slf4j
+@RequiredArgsConstructor
+public class TestDataInit {
+    private final ReservationRepository reservationRepository;
+
+    /*테스트용 초기 데이터*/
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initData() {
+        log.info("TEST DATE INIT");
+
+        reservationRepository.save(new Reservation(306L,1L,1L, State.RESERVATION));
+         reservationRepository.save(new Reservation(428L,2L,2L, State.VISITED));
+    }
+
+}
