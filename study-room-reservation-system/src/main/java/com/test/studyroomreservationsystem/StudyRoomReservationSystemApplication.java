fd499645dbdb6c46e9e8a3a284fb79e088bc592d
HwangBBang <h1009218@hufs.ac.kr>
2024-02-26 18:23:32 +0900

    add local profile

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/StudyRoomReservationSystemApplication.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/StudyRoomReservationSystemApplication.java
@@ -1,13 +1,24 @@
 package com.test.studyroomreservationsystem;
 
+import com.test.studyroomreservationsystem.config.JpaConfig;
+import com.test.studyroomreservationsystem.repository.ReservationRepository;
+import com.test.studyroomreservationsystem.repository.jpa.JpaReservationRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
+@Import(JpaConfig.class)
 @SpringBootApplication
 public class StudyRoomReservationSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudyRoomReservationSystemApplication.class, args);
 	}
-
+	@Bean
+	@Profile("local")
+	public TestDataInit testDataInit(ReservationRepository reservationRepository){
+		return new TestDataInit(reservationRepository);
+	}
 }
