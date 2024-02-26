3741d105fb2f8d3440d977a685d09e85a1bbd184
HwangBBang <h1009218@hufs.ac.kr>
2024-02-26 20:46:58 +0900

    rename class

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/config/JpaConfig.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/config/JpaConfig.java
@@ -1,20 +1,27 @@
 package com.test.studyroomreservationsystem.config;
 
 import com.test.studyroomreservationsystem.repository.ReservationRepository;
-import com.test.studyroomreservationsystem.repository.jpa.JpaReservationRepo;
+import com.test.studyroomreservationsystem.repository.jpa.JpaReservationRepository;
 import com.test.studyroomreservationsystem.service.ReservationService;
 import com.test.studyroomreservationsystem.service.ReservationServiceV1;
+import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JpaConfig {
+    private EntityManager em;
+
+    public JpaConfig(EntityManager em) {
+        this.em = em;
+    }
+
     @Bean
     public ReservationService reservationService(){
         return new ReservationServiceV1(reservationRepository());
     }
     @Bean
     public ReservationRepository reservationRepository(){
-        return new JpaReservationRepo();
+        return new JpaReservationRepository(em);
     }
 }
