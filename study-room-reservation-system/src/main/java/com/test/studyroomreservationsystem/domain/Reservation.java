1e4076599d3634f080d6ceefa044ab04c273ae45
HwangBBang <h1009218@hufs.ac.kr>
2024-02-26 20:47:16 +0900

    fixed domain

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/domain/Reservation.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/domain/Reservation.java
@@ -1,19 +1,26 @@
 package com.test.studyroomreservationsystem.domain;
 
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name="Reservation")
 public class Reservation {
+
+    // MySQL pk 값 설정 위임하기
+    @Id // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reservationId;
+
+//    @Column
     private Long roomId;
     private Long partitionId;
-    private Long reservationId;
     private Enum<State> state;
 
 
 
-    public Reservation() {
-    }
+    public Reservation() {}
 
     public Reservation(Long roomId, Long partitionId, Long reservationId, Enum<State> state) {
         this.roomId = roomId;
