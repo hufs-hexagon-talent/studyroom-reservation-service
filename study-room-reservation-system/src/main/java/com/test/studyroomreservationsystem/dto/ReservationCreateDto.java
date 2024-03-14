aed70cc0fbca56ff334393cd871079989f43b0c7
HwangBBang <h1009218@hufs.ac.kr>
2024-03-14 17:57:05 +0900

    make dtos

---
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/dto/ReservationCreateDto.java
@@ -0,0 +1,16 @@
+package com.test.studyroomreservationsystem.dto;
+
+import com.test.studyroomreservationsystem.domain.ReservationState;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+public class ReservationCreateDto { // CR
+    private Long userId;
+    private Long roomId;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+    private ReservationState state;
+}
