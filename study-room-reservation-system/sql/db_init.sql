34dcad1d425bfdb3aae9c3994849cd78386b9f22
HwangBBang <h1009218@hufs.ac.kr>
2024-03-14 17:51:58 +0900

    Update db_init.sql

--- study-room-reservation-system/sql/db_init.sql
+++ study-room-reservation-system/sql/db_init.sql
@@ -4,27 +4,16 @@ ALTER TABLE `reservation` DROP FOREIGN KEY `FK_user_reservation`;
 ALTER TABLE `timetable` DROP FOREIGN KEY `FK_part_timetable`;
 
 DROP TABLE IF EXISTS `timetable`;
-DROP TABLE IF EXISTS `timetableconfig`;
 DROP TABLE IF EXISTS `user` ;
 DROP TABLE IF EXISTS `reservation`;
-DROP TABLE IF EXISTS `partition`;
 DROP TABLE IF EXISTS `room`;
 
 -- room Table
 CREATE TABLE `room` (
 	`room_id` INT NOT NULL AUTO_INCREMENT,
-	`timetable_config_id` BIGINT NOT NULL,
+    `room_name` CHAR(50),
+	`timetable_id` BIGINT NOT NULL,
 	PRIMARY KEY (`room_id`)
-);
-
--- partition Table
-
-
-CREATE TABLE `partition`(
-	`partition_id` INT NOT NULL AUTO_INCREMENT,
-	`timetable_config_id` BIGINT	,
-	`room_id` INT	,
-	PRIMARY KEY (`partition_id`)
 );
 
 -- reservation Table
@@ -32,11 +21,8 @@ CREATE TABLE `partition`(
 CREATE TABLE `reservation` (
 	`reservation_id` BIGINT NOT NULL AUTO_INCREMENT,
 	`user_id` INT NOT NULL,
-	`partition_id` INT	,
 	`timetable_id`	BIGINT,
 	`room_id`	INT	,
-	`timetable_index_from`	INT	,
-	`timetable_index_to`	INT	,
 	`state` ENUM ('RESERVED', 'VISITED', 'NOSHOW' ),
 	PRIMARY KEY(`reservation_id`)
 );
@@ -45,35 +31,33 @@ CREATE TABLE `reservation` (
 
 CREATE TABLE `user` (
 	`user_id` INT NOT NULL, -- 테스트용 학번 
-	`username`	VARCHAR(30) UNIQUE, 
+	`loginid`	VARCHAR(30) UNIQUE, 
 	`password`	VARCHAR(255),
 	`serial` CHAR(20) UNIQUE,
 	`is_admin`	BOOLEAN	,
 	PRIMARY KEY(`user_id`)
 );
 
--- timetableConfig Table
-
-CREATE TABLE `timetableconfig` (
-	`timetable_config_id` BIGINT NOT NULL AUTO_INCREMENT,
-	`start_time` TIME,
-	`end_time` TIME,
-	`interval_minute`	INT	,
-	`max_slots` INT,
-	PRIMARY KEY(`timetable_config_id`)
-);
-
--- timetableConfig Table
-
+-- 해당 데이트에 대한 제한
+-- 몇 시부터 몇시까지
+-- 한사람이 몇분간 까지 연속으로 배정이 가능한지
 CREATE TABLE `timetable` (
-	`timetable_id` BIGINT NOT NULL AUTO_INCREMENT,
-	`partition_id` INT	,
-	`start_time`	TIME ,
-	`end_time`	TIME ,
-	`interval_minute`	INT	,
-	`max_slots`	INT,
-	PRIMARY KEY(`timetable_id`)
+     `timetable_id` BIGINT NOT NULL AUTO_INCREMENT,
+     `start_time`	TIME ,
+     `end_time`	TIME ,
+     `each_max_minute`	INT,
+     PRIMARY KEY(`timetable_id`)
 );
+
+-- 래퍼런스로가지고 아직 생기지않은 몇일 후 까지의 타임테이블을 만들어줌
+-- CREATE TABLE `timetableReference` (
+-- 	`timetable_referce_id` BIGINT NOT NULL AUTO_INCREMENT,
+-- 	`` DATE,
+-- 	PRIMARY KEY(`timetable_config_id`)
+-- );
+
+
+
 
 
 
