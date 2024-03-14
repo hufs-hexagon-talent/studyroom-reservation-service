af628592b287db562cd7fa1f1836774b79fb8a60
HwangBBang <h1009218@hufs.ac.kr>
2024-03-14 17:54:09 +0900

    create room entity

---
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/domain/entity/Room.java
@@ -0,0 +1,23 @@
+package com.test.studyroomreservationsystem.domain.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+@Table(name="room")
+public class Room {
+    @Id
+    private Long roomId;
+
+    private String roomName;
+
+    @ManyToOne
+    @JoinColumn(name = "room_operation_policy_id")
+    private RoomOperationPolicy roomOperationPolicy;
+
+}
+
+
