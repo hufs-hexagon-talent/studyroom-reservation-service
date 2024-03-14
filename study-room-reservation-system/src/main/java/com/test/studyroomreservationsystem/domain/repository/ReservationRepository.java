5a775a4db7f723b5c0d6bb17a7cab58f9c5fe12a
HwangBBang <h1009218@hufs.ac.kr>
2024-03-15 02:22:50 +0900

    create findOverlappingReservations method

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/domain/repository/ReservationRepository.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/domain/repository/ReservationRepository.java
@@ -2,8 +2,23 @@ package com.test.studyroomreservationsystem.domain.repository;
 
 import com.test.studyroomreservationsystem.domain.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
     // JpaRepository 에서 인터페이스 CrudRepository 가 기본 CRUD 기능을 제공
+
+    @Query("SELECT r " +
+            "FROM Reservation r " +
+            "WHERE r.room.roomId = :roomId AND (r.startDateTime < :endTime AND r.endDateTime > :startTime)"
+    )
+    List<Reservation> findOverlappingReservations( @Param("roomId") Long roomId,
+                                                  @Param("startTime") LocalDateTime startTime,
+                                                  @Param("endTime") LocalDateTime endTime
+    );
+
 }
