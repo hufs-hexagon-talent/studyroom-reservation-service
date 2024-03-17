b5f4308bdc3932a88ceeae467e3c163a51075222
HwangBBang <h1009218@hufs.ac.kr>
2024-03-17 15:44:48 +0900

    add find methods

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/domain/repository/UserRepository.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/domain/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.test.studyroomreservationsystem.domain.repository;
 import com.test.studyroomreservationsystem.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByLoginId(String loginId);
+    Optional<User> findBySerial(String serial);
 }
