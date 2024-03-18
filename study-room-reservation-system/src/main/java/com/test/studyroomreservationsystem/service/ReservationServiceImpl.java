6b4f452e251270e0e21c288162b7b0203451f3a0
HwangBBang <h1009218@hufs.ac.kr>
2024-03-18 23:33:52 +0900

    add findAllReservationByUser service

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/service/ReservationServiceImpl.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/service/ReservationServiceImpl.java
@@ -8,6 +8,7 @@ import com.test.studyroomreservationsystem.dto.reservation.ReservationDto;
 import com.test.studyroomreservationsystem.dto.reservation.ReservationRoomDto;
 import com.test.studyroomreservationsystem.dto.reservation.ReservationStateDto;
 import com.test.studyroomreservationsystem.dto.reservation.ReservationTimeDto;
+import com.test.studyroomreservationsystem.service.exception.ReservationHistoryNotFoundException;
 import com.test.studyroomreservationsystem.service.exception.ReservationNotFoundException;
 import com.test.studyroomreservationsystem.service.exception.ReservationNotPossibleException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,6 +66,14 @@ public class ReservationServiceImpl implements ReservationService {
         return reservationRepository.findAll();
     }
 
+    // 해당 유저의 모든 reservation 로그 찾기
+    @Override
+    public List<Reservation> findAllReservationByUser(Long userId) {
+        User user = userService.findUserById(userId);
+        return reservationRepository.findAllByUser(user).orElseThrow(
+                () -> new ReservationHistoryNotFoundException("Reservation History not found with user id: " + userId));
+    }
+
     @Override
     public Reservation updateReservation(Long reservationId, ReservationDto reservationDto) {
         Long roomId = reservationDto.getRoomId();
@@ -93,7 +102,7 @@ public class ReservationServiceImpl implements ReservationService {
         return reservationRepository.save(reservation);
     }
 
-    // 현재 예약에서 시간만 변경
+    // 현재 예약 에서 시간만 변경
     @Override
     public Reservation updateTimeReservation(Long reservationId, ReservationTimeDto timeDto) {
         Reservation reservation = findReservationById(reservationId);
@@ -113,7 +122,7 @@ public class ReservationServiceImpl implements ReservationService {
         return reservationRepository.save(reservation);
     }
 
-    // 현재 예약에서 상태만 변경
+    // 현재 예약 에서 상태만 변경
     @Override
     public Reservation updateStateReservation(Long reservationId, ReservationStateDto stateDto) {
         Reservation reservation = findReservationById(reservationId);
@@ -148,10 +157,10 @@ public class ReservationServiceImpl implements ReservationService {
 
 
     private boolean isReservationAvailable(Long roomId,LocalDateTime startDateTime, LocalDateTime endDateTime) {
-        // 룸의 운영 시간인지 확인
+        // 룸의 운영 시간 인지 확인
         boolean isOperating = roomService.isRoomAvailable(roomId, startDateTime, endDateTime);
 
-        // 다른 예약과 겹치지 않는지 확인
+        // 다른 예약과 겹치지 않는지 확인 (계층)
         boolean isNotOverlapping = reservationRepository.findOverlappingReservations(roomId, startDateTime, endDateTime).isEmpty();
 
         return isOperating && isNotOverlapping;
