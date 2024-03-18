c3d91bb3d14ff35a6b65cdb3787f9a496461ca5e
HwangBBang <h1009218@hufs.ac.kr>
2024-03-18 23:33:17 +0900

    Create ReservationHistoryNotFoundException.java

---
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/service/exception/ReservationHistoryNotFoundException.java
@@ -0,0 +1,8 @@
+package com.test.studyroomreservationsystem.service.exception;
+
+public class ReservationHistoryNotFoundException extends RuntimeException {
+    public ReservationHistoryNotFoundException(String message) {
+        super(message);
+    }
+
+}
