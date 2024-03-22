2d27a136c69c24aaa630eb892241eb2e840e3718
HwangBBang <h1009218@hufs.ac.kr>
2024-03-22 18:13:46 +0900

    Separated APIController about User

---
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/apicontroller/user/UserController.java
@@ -0,0 +1,44 @@
+package com.test.studyroomreservationsystem.apicontroller.user;
+
+import com.test.studyroomreservationsystem.domain.entity.User;
+import com.test.studyroomreservationsystem.dto.user.UserDto;
+import com.test.studyroomreservationsystem.dto.user.UserUpdateDto;
+import com.test.studyroomreservationsystem.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Tag(name = "User", description = "사용자 관련 API")
+@RestController
+@RequestMapping("/user/users")
+public class UserController {
+    private final UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Operation(summary = "user 조회", description = "본인 정보 조회 API")
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long userId) {
+        User foundUser = userService.findUserById(userId);
+        UserDto user = userService.convertToDto(foundUser);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @Operation(summary = "user 정보 업데이트", description = "본인 정보 업데이트 API")
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long userId, @RequestBody UserUpdateDto userUpdateDto) {
+        User updatedUser = userService.updateUser(userId, userUpdateDto);
+        UserDto user = userService.convertToDto(updatedUser);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+
+}
