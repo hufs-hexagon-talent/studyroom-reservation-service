4bad7bceb1ff2cace12ec580d3ad3880ab51fffd
HwangBBang <h1009218@hufs.ac.kr>
2024-04-02 14:02:27 +0900

    edit user dto
    add field

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/dto/user/UserDto.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/dto/user/UserDto.java
@@ -1,6 +1,7 @@
 package com.test.studyroomreservationsystem.dto.user;
 
 
+import com.test.studyroomreservationsystem.domain.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,4 +11,18 @@ public class UserDto { // CR dto
     private String password;
     private String serial;
     private Boolean isAdmin;
+    private String userName;
+
+
+// of :  Dto -> Entity
+// from :  Entity -> Dto
+
+//    public UserDto of(User user) {
+//
+//        return
+//    }
+//
+//    public User from(UserDto userDto) {
+//        return
+//    }
 }
