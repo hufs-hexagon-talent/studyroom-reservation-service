97a1f89990562d141303426a59ac9508b21a13f7
HwangBBang <h1009218@hufs.ac.kr>
2024-04-02 14:00:42 +0900

    create new Exception

---
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/service/exception/RoomPolicyNotFoundException.java
@@ -0,0 +1,13 @@
+package com.test.studyroomreservationsystem.service.exception;
+
+
+public class RoomPolicyNotFoundException extends RuntimeException {
+    public RoomPolicyNotFoundException(String message) {
+        super(message);
+    }
+}
+
+
+
+
+
