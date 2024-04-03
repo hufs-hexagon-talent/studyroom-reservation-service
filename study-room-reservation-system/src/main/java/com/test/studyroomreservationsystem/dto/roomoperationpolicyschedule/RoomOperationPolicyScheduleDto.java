4ecae7c684b2d3ebff2b3f16a37b848c0c1deb56
HwangBBang <h1009218@hufs.ac.kr>
2024-04-03 13:13:18 +0900

    update schedule dto
    change field

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/dto/roomoperationpolicyschedule/RoomOperationPolicyScheduleDto.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/dto/roomoperationpolicyschedule/RoomOperationPolicyScheduleDto.java
@@ -1,6 +1,5 @@
 package com.test.studyroomreservationsystem.dto.roomoperationpolicyschedule;
 
-import com.test.studyroomreservationsystem.domain.entity.Room;
 import com.test.studyroomreservationsystem.domain.entity.RoomOperationPolicy;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,9 +8,8 @@ import java.time.LocalDate;
 
 @Getter@Setter
 public class RoomOperationPolicyScheduleDto {
-    private Long roomOperationPolicyScheduleId;
-    private Room room;
-    private RoomOperationPolicy roomOperationPolicy;
+    private Long roomId;
+    private Long roomOperationPolicyId;
     private LocalDate policyApplicationDate;
 
 }
