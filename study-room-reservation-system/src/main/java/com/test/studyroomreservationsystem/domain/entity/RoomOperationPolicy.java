70d5f1b4c178233c1e7382c76413032ebb784753
HwangBBang <h1009218@hufs.ac.kr>
2024-04-05 19:27:56 +0900

    apply @Builder at entity & implement  the Entity to DTO ,  DTO to Entity method in dto-layer
    about RoomOperationPolicy entity

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/domain/entity/RoomOperationPolicy.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/domain/entity/RoomOperationPolicy.java
@@ -3,7 +3,9 @@ package com.test.studyroomreservationsystem.domain.entity;
 import jakarta.persistence.*;
 
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalTime;
@@ -11,6 +13,7 @@ import java.time.LocalTime;
 
 @Getter @Setter
 @Entity
+@NoArgsConstructor
 @Table(name = "roomOperationPolicy")
 public class RoomOperationPolicy {
 
@@ -27,4 +30,12 @@ public class RoomOperationPolicy {
 
     @Column(name="each_max_minute")
     private Integer eachMaxMinute;
+
+    @Builder
+    public RoomOperationPolicy(Long roomOperationPolicyId, LocalTime operationStartTime, LocalTime operationEndTime, Integer eachMaxMinute) {
+        this.roomOperationPolicyId = roomOperationPolicyId;
+        this.operationStartTime = operationStartTime;
+        this.operationEndTime = operationEndTime;
+        this.eachMaxMinute = eachMaxMinute;
+    }
 }
--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/dto/roomoperationpolicy/RoomOperationPolicyDto.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/dto/roomoperationpolicy/RoomOperationPolicyDto.java
@@ -1,12 +1,30 @@
 package com.test.studyroomreservationsystem.dto.roomoperationpolicy;
 
+import com.test.studyroomreservationsystem.domain.entity.RoomOperationPolicy;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
 
 @Getter
+@NoArgsConstructor
 public class RoomOperationPolicyDto {
     private LocalTime operationStartTime;
     private LocalTime operationEndTime;
     private Integer eachMaxMinute;
+
+    @Builder
+    public RoomOperationPolicyDto(LocalTime operationStartTime, LocalTime operationEndTime, Integer eachMaxMinute) {
+        this.operationStartTime = operationStartTime;
+        this.operationEndTime = operationEndTime;
+        this.eachMaxMinute = eachMaxMinute;
+    }
+    public RoomOperationPolicy toEntity() {
+        return RoomOperationPolicy.builder()
+                .operationStartTime(operationStartTime)
+                .operationEndTime(operationEndTime)
+                .eachMaxMinute(eachMaxMinute)
+                .build();
+    }
 }
