ed63137e08c8217ca3c1881997cf33bbbb179d71
HwangBBang <h1009218@hufs.ac.kr>
2024-04-07 12:14:51 +0900

    update user service layer
    create default method entity to dto

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/service/UserService.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/service/UserService.java
@@ -3,6 +3,7 @@ package com.test.studyroomreservationsystem.service;
 import com.test.studyroomreservationsystem.domain.entity.User;
 import com.test.studyroomreservationsystem.dto.user.UserDto;
 import com.test.studyroomreservationsystem.dto.user.UserUpdateDto;
+import lombok.Builder;
 
 import java.util.List;
 
@@ -12,4 +13,13 @@ public interface UserService {
     List<User> findAllUsers();
     User updateUser(Long userId, UserUpdateDto userUpdateDto);
     void deleteUser(Long userId);
+    default UserDto toDto(User user) {
+        return UserDto.builder()
+                .loginId(user.getLoginId())
+                .password(user.getPassword())
+                .serial(user.getSerial())
+                .isAdmin(user.getIsAdmin())
+                .userName(user.getUserName())
+                .build();
+    }
 }
