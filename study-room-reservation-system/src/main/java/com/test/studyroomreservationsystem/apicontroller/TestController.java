9c4b84d8125094f385da2ae870ce4dfc4061d0c0
HwangBBang <h1009218@hufs.ac.kr>
2024-04-10 23:46:35 +0900

    edit testController

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/apicontroller/TestController.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/apicontroller/TestController.java
@@ -1,2 +1,17 @@
-package com.test.studyroomreservationsystem.apicontroller;public class TestController {
+package com.test.studyroomreservationsystem.apicontroller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    ResponseEntity<Object> testApi() {
+        String info = "Successful API connection";
+        return new ResponseEntity<>(info,HttpStatus.OK );
+
+    }
+
 }
