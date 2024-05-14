87eb72d518324597cbb0d168880d159726d0fbb0
HwangBBang <h1009218@hufs.ac.kr>
2024-05-14 16:44:07 +0900

    fixed init.d

--- study-room-reservation-system/mysql/init.d/initschema.sql
+++ study-room-reservation-system/mysql/init.d/initschema.sql
@@ -1,73 +1,82 @@
-create table `service-db`.room
+create table refresh
+(
+    id         bigint auto_increment
+        primary key,
+    expiration varchar(255) null,
+    refresh    varchar(255) null,
+    username   varchar(255) null
+);
+
+create table room
 (
     room_id   bigint auto_increment
         primary key,
     room_name varchar(255) null
 );
 
-create table `service-db`.room_operation_policy
+create table room_operation_policy
 (
-    room_operation_policy_id bigint auto_increment
-        primary key,
     each_max_minute          int     null,
     operation_end_time       time(6) null,
-    operation_start_time     time(6) null
+    operation_start_time     time(6) null,
+    room_operation_policy_id bigint auto_increment
+        primary key
 );
 
-create table `service-db`.room_operation_policy_schedule
+create table room_operation_policy_schedule
 (
-    room_operation_policy_schedule_id bigint auto_increment
-        primary key,
     policy_application_date           date   null,
     room_id                           bigint null,
     room_operation_policy_id          bigint null,
+    room_operation_policy_schedule_id bigint auto_increment
+        primary key,
     constraint UKpfqbat5pgysdgfkha5fyggpi7
         unique (room_id, policy_application_date),
     constraint FK69q6uql7yr8nj2be1s7hduk8j
-        foreign key (room_operation_policy_id) references `service-db`.room_operation_policy (room_operation_policy_id),
+        foreign key (room_operation_policy_id) references room_operation_policy (room_operation_policy_id),
     constraint FKtqf4du6n3yxxy8bg6wwgyq0iq
-        foreign key (room_id) references `service-db`.room (room_id)
+        foreign key (room_id) references room (room_id)
 );
 
-create table `service-db`.user
+create table user
 (
-    user_id   bigint auto_increment
+    is_admin bit          null,
+    user_id  bigint auto_increment
         primary key,
-    is_admin  bit          null,
-    login_id  varchar(255) null,
-    password  varchar(255) null,
-    serial    varchar(255) null,
-    user_name varchar(255) null,
-    constraint UK_6ntlp6n5ltjg6hhxl66jj5u0l
-        unique (login_id),
+    serial   varchar(9)   null,
+    name     varchar(255) null,
+    password varchar(255) null,
+    username varchar(255) null,
     constraint UK_itinxgefg8bjyffgxwwmc2oaa
-        unique (serial)
+        unique (serial),
+    constraint UK_sb8bbouer5wak8vyiiy4pf2bx
+        unique (username)
 );
 
-create table `service-db`.reservation
+create table reservation
 (
+    reservation_end_time   datetime(6)                            null,
     reservation_id         bigint auto_increment
         primary key,
-    reservation_end_time   datetime(6)                               null,
-    reservation_start_time datetime(6)                               null,
-    state                  enum ('RESERVATION', 'VISITED', 'NOSHOW') null,
-    room_id                bigint                                    null,
-    user_id                bigint                                    null,
+    reservation_start_time datetime(6)                            null,
+    room_id                bigint                                 null,
+    user_id                bigint                                 null,
+    state                  enum ('RESERVED', 'VISITED', 'NOSHOW') null,
     constraint FKm4oimk0l1757o9pwavorj6ljg
-        foreign key (user_id) references `service-db`.user (user_id),
+        foreign key (user_id) references user (user_id),
     constraint FKm8xumi0g23038cw32oiva2ymw
-        foreign key (room_id) references `service-db`.room (room_id)
+        foreign key (room_id) references room (room_id)
 );
 
 -- data 삽입
 INSERT INTO `service-db`.`user`
-(user_name, login_id, password, serial, is_admin)
+(name, username, password, serial, is_admin)
 VALUES
-    ('관리자','admin', 'admin', '123456789' , TRUE),
-    ('황병훈','h1009218', 'h1009218', '202103769',  TRUE),
-    ('김지섭','0226daniel', '0226daniel', '202012345',  TRUE),
-    ('이서연','lsy0476', 'lsy0476', '202202465',  FALSE),
-    ('김소정','sojeong22', 'sojeong22', '202200720',  FALSE);
+    ('관리자','admin', '$2a$10$8M0z/VSeWXOTO8LXTimtZeK6YGTdNNRca1nFgDAFQvZoj1ZYc2bH2', '123456789' , TRUE),
+    ('황병훈','h1009218', '$2a$10$5UF4pm1p5/i73rvxVGq4z.e8y0EUKpxdJHr2e5MshaNM5kXxCOjne', '202103769',  TRUE),
+    ('김지섭','0226daniel', '$2a$10$GAmvgYVVlBq3.20i/hB/AOdarXyl3a8y8BvqXhEjuTJvYWGzfAJE2', '202012345',  TRUE),
+    ('이서연','lsy0476', '$2a$10$mMpahAEAKv.eJTrEhlVsvu4tFvS7VsqYvqXnBZoYZ5lvEDKmCBarS', '202202465',  TRUE),
+    ('김소정','sojeong22', '$2a$10$zQ/HRLZgQF5RRH0jiDzhgeg1u5otgXpvNLx2.MKJ8EqA77US7VZzu', '202200720',  TRUE);
 
 INSERT INTO `service-db`.`room_operation_policy` (operation_start_time, operation_end_time, each_max_minute)
 VALUES
@@ -89,13 +98,14 @@ VALUES
 
 INSERT INTO `service-db`.`room_operation_policy_schedule` (room_id, room_operation_policy_id, policy_application_date)
 VALUES
-    (1, 1, '2024-04-17'),
-    (2, 1, '2024-04-17'),
-    (3, 1, '2024-04-17'),
-    (4, 1, '2024-04-17'),
-    (5, 1, '2024-04-17'),
-    (6, 1, '2024-04-17');
+    (1, 1, '2024-05-11'),
+    (2, 1, '2024-05-11'),
+    (3, 1, '2024-05-11'),
+    (4, 1, '2024-05-11'),
+    (5, 1, '2024-05-11'),
+    (6, 1, '2024-05-11');
 
 INSERT INTO `service-db`.`reservation` (user_id, room_id, reservation_start_time, reservation_end_time, state)
 VALUES
-    (2, 1, '2024-05-04 09:00:00', '2024-05-04 10:00:00', 'RESERVED');
+    (2, 1, '2024-05-04 09:00:00', '2024-05-11 10:00:00', 'RESERVED');
+
