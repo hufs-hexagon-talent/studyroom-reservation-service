e388f25999c44f0e4072b9d38571c1a2ca0dedb0
HwangBBang <h1009218@hufs.ac.kr>
2024-05-19 22:37:31 +0900

    fixed cors config

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/config/SecurityConfig.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/config/SecurityConfig.java
@@ -83,7 +83,7 @@ public class SecurityConfig {
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 
                         CorsConfiguration configuration = new CorsConfiguration();
-                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        configuration.setAllowedOrigins(Collections.singletonList("*"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
