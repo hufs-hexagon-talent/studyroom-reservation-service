86a0844c8aded2e4966e5a3b9689205e3f620ea6
HwangBBang <h1009218@hufs.ac.kr>
2024-05-24 02:33:54 +0900

    Remove unused exception class

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/exception/AuthorizationTokenMissingException.java
+++
@@ -1,7 +0,0 @@
-package com.test.studyroomreservationsystem.exception;
-
-public class AuthorizationTokenMissingException extends RuntimeException {
-    public AuthorizationTokenMissingException(String claimsId) {
-        super(claimsId + " : 권한 정보가 없는 토큰입니다.");
-    }
-}
