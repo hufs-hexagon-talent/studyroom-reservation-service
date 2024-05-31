7295ddc2339d7739eb484900631c2d96e5593a32
HwangBBang <h1009218@hufs.ac.kr>
2024-05-31 22:34:22 +0900

    [feat] Add Redis integration and config settings

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/config/RedisConfig.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/config/RedisConfig.java
@@ -1,2 +1,42 @@
-package com.test.studyroomreservationsystem.config;public class RedisConfig {
+package com.test.studyroomreservationsystem.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Value("${spring.data.redis.host}") private String host;
+
+    @Value("${spring.data.redis.port}") private int port;
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        // Redis를 연결합니다.
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        // Key-Value 형태로 직렬화를 수행합니다.
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        // Hash Key-Value 형태로 직렬화를 수행합니다.
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+
+        // 기본적으로 직렬화를 수행합니다.
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
 }
