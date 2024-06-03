8d92d4f1df0f751aa3295b810808981bd4027742
HwangBBang <h1009218@hufs.ac.kr>
2024-06-03 17:20:04 +0900

    [feat] rename controller

--- study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/apicontroller/user/OTPCodeController.java
+++ study-room-reservation-system/src/main/java/com/test/studyroomreservationsystem/apicontroller/user/OTPCodeController.java
@@ -13,25 +13,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Duration;
 import java.time.Instant;
-@Tag(name = "QR", description = "QR 정보 관련 API")
+@Tag(name = "OTP", description = "OTP 제공 관련 API")
 @RestController
-@RequestMapping("/qr")
-public class QRCodeController {
+@RequestMapping("/otp")
+public class OTPCodeController {
     private final RedisService redisService;
     private final QRCodeService qrCodeService;
 
-    public QRCodeController(RedisService redisService, QRCodeService qrCodeService) {
+    public OTPCodeController(RedisService redisService, QRCodeService qrCodeService) {
         this.redisService = redisService;
         this.qrCodeService = qrCodeService;
     }
 
-    @Operation(summary = "✅ QR code 생성",
-            description = "로그인된 유저에 대해서 QR 코드 생성",
+    @Operation(summary = "✅ QR code 생성, OTP 제공",
+            description = "로그인된 유저에 대해서 OTP 제공",
             security = {@SecurityRequirement(name = "JWT")}
     )
     @PostMapping
