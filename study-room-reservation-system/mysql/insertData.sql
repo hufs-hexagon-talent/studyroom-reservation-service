af9d8bd7bb003f76090473af93ecc89306e4d640
HwangBBang <h1009218@hufs.ac.kr>
2024-07-25 14:32:24 +0900

    [fix] edit insert db

--- study-room-reservation-system/mysql/insertData.sql
+++ study-room-reservation-system/mysql/insertData.sql
@@ -1,13 +1,12 @@
 -- data 삽입
 INSERT INTO `service-db`.`user`
-(name, username, password, serial, is_admin, email)
+(name, username, password, serial, email, is_admin)
 VALUES
-    ('관리자','admin', '$2a$10$8M0z/VSeWXOTO8LXTimtZeK6YGTdNNRca1nFgDAFQvZoj1ZYc2bH2', '123456789' , TRUE, 'hwangbbang9@gmail.com'),
-    ('황병훈','h1009218', '$2a$10$5UF4pm1p5/i73rvxVGq4z.e8y0EUKpxdJHr2e5MshaNM5kXxCOjne', '202103769',  TRUE, 'h1009218@hufs.ac.kr'),
-    ('김지섭','0226daniel', '$2a$10$GAmvgYVVlBq3.20i/hB/AOdarXyl3a8y8BvqXhEjuTJvYWGzfAJE2', '202012345',  TRUE, '0226daniel@hufs.ac.kr'),
-    ('이서연','lsy0476', '$2a$10$mMpahAEAKv.eJTrEhlVsvu4tFvS7VsqYvqXnBZoYZ5lvEDKmCBarS', '202202465',  TRUE, 'lsy0476@hufs.ac.kr'),
-    ('김소정','sojeong22', '$2a$10$zQ/HRLZgQF5RRH0jiDzhgeg1u5otgXpvNLx2.MKJ8EqA77US7VZzu', '202200720',  TRUE, 'sojeong22@hufs.ac.kr');
-
+    ('관리자','admin', '$2a$10$8M0z/VSeWXOTO8LXTimtZeK6YGTdNNRca1nFgDAFQvZoj1ZYc2bH2', '123456789' ,'hwangbbang9@gmail.com', TRUE),
+    ('황병훈','h1009218', '$2a$10$5UF4pm1p5/i73rvxVGq4z.e8y0EUKpxdJHr2e5MshaNM5kXxCOjne', '202103769','h1009218@hufs.ac.kr',  TRUE),
+    ('김지섭','0226daniel', '$2a$10$GAmvgYVVlBq3.20i/hB/AOdarXyl3a8y8BvqXhEjuTJvYWGzfAJE2', '202012345', '0226daniel@hufs.ac.kr',  TRUE),
+    ('이서연','lsy0476', '$2a$10$mMpahAEAKv.eJTrEhlVsvu4tFvS7VsqYvqXnBZoYZ5lvEDKmCBarS', '202202465', 'lsy0476@@hufs.ac.kr', TRUE ),
+    ('김소정','sojeong22', '$2a$10$zQ/HRLZgQF5RRH0jiDzhgeg1u5otgXpvNLx2.MKJ8EqA77US7VZzu', '202200720', 'sojeong22@hufs.ac.kr', TRUE );
 
 INSERT INTO `service-db`.`room_operation_policy` (operation_start_time, operation_end_time, each_max_minute)
 VALUES
@@ -17,14 +16,12 @@ VALUES
     ('09:00:00', '22:00:00', 60),
     ('09:00:00', '22:00:00', 120),
     ('09:00:00', '22:00:00', 180),
-    ('00:00:00', '23:59:59', 60);
+    ('09:00:00', '23:59:59', 60);
 
 INSERT INTO `service-db`.`room` (room_name)
 VALUES
     ('306'),
     ('428');
-
-
 INSERT INTO `service-db`.`room_partition` (room_id, partition_number)
 VALUES
     (1,'1'),
