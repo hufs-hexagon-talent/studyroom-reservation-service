93f33bd7d7d091455fa0c289081fb109c5cb02be
HwangBBang <h1009218@hufs.ac.kr>
2024-10-05 10:43:35 +0900

    [refactor] Introduce mapper interfaces for DTO conversion refactoring by domain

---
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/department/mapper/DepartmentMapper.java
@@ -0,0 +1,7 @@
+package hufs.computer.studyroom.domain.department.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface DepartmentMapper {
+}
---
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/reservation/mapper/ReservationMapper.java
@@ -0,0 +1,7 @@
+package hufs.computer.studyroom.domain.reservation.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ReservationMapper {
+}
