e6bcdefe4febe64bdc94c66e1e3d82694d1dc30d
HwangBBang <h1009218@hufs.ac.kr>
2024-10-08 16:35:15 +0900

    [refactor] Refactor ModifyOperationPolicyRequest from class to record

--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/policy/dto/request/ModifyOperationPolicyRequest.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/policy/dto/request/ModifyOperationPolicyRequest.java
@@ -1,11 +1,17 @@
 package hufs.computer.studyroom.domain.policy.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalTime;
-@Getter
-public class ModifyOperationPolicyRequest {
-    private LocalTime operationStartTime;
-    private LocalTime operationEndTime;
-    private Integer eachMaxMinute;
+
+@Schema
+@Builder
+public record ModifyOperationPolicyRequest (
+        @Schema() LocalTime operationStartTime,
+        @Schema() LocalTime operationEndTime,
+        @Schema() Integer eachMaxMinute
+){
+
 }
