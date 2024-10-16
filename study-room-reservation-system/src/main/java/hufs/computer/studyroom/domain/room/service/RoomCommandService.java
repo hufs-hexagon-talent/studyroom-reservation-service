127649e434a5de05c84377e074aab2ec87a1e65f
HwangBBang <h1009218@hufs.ac.kr>
2024-10-16 17:53:41 +0900

    [refactor] Apply CQRS pattern to Room domain service

---
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/room/service/RoomCommandService.java
@@ -0,0 +1,43 @@
+package hufs.computer.studyroom.domain.room.service;
+
+import hufs.computer.studyroom.common.service.CommonHelperService;
+import hufs.computer.studyroom.domain.department.entity.Department;
+import hufs.computer.studyroom.domain.room.dto.request.CreateRoomRequest;
+import hufs.computer.studyroom.domain.room.dto.request.ModifyRoomRequest;
+import hufs.computer.studyroom.domain.room.dto.response.RoomInfoResponse;
+import hufs.computer.studyroom.domain.room.entity.Room;
+import hufs.computer.studyroom.domain.room.mapper.RoomMapper;
+import hufs.computer.studyroom.domain.room.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RoomCommandService {
+    private final RoomRepository roomRepository;
+    private final RoomMapper roomMapper;
+    private final CommonHelperService commonHelperService;
+
+    public RoomInfoResponse createRoom(CreateRoomRequest request) {
+        Department department = commonHelperService.getDepartmentById(request.departmentId());
+        Room room = roomMapper.toRoom(request, department);
+        roomRepository.save(room);
+        return roomMapper.toInfoResponse(room);
+    }
+
+    public RoomInfoResponse updateRoom(Long roomId, ModifyRoomRequest request) {
+        Room room = commonHelperService.getRoomById(roomId);
+        Department department = commonHelperService.getDepartmentById(request.departmentId());
+        Room modified = roomMapper.updateRoom(request, department, room);
+
+        roomRepository.save(modified);
+        return roomMapper.toInfoResponse(room);
+    }
+
+    public void deleteRoom(Long roomId) {
+        commonHelperService.getRoomById(roomId);
+        roomRepository.deleteById(roomId);
+    }
+}
