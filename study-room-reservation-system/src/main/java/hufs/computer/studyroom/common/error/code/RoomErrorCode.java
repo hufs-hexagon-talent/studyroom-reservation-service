4120d30bcaf2e4ff780b9bd94f569bf3157c38fc
HwangBBang <h1009218@hufs.ac.kr>
2024-10-22 21:40:01 +0900

    [refactor] Add CheckIn domain mapper layer and refactor service and controller layers

--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/common/error/code/RoomErrorCode.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/common/error/code/RoomErrorCode.java
@@ -9,7 +9,9 @@ public enum RoomErrorCode implements ErrorCode {
     ROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "ROOM-001", "해당 방은 존재하지 않습니다."),
     ROOM_ALREADY_EXISTS(HttpStatus.CONFLICT, "ROOM-002", "이미 존재하는 방입니다."),
     PARTITION_NOT_FOUND(HttpStatus.NOT_FOUND, "ROOM-003", "해당 파티션은 존재하지 않습니다."),
-    PARTITION_ALREADY_EXISTS(HttpStatus.CONFLICT, "ROOM-004", "이미 존재하는 파티션입니다.");
+    PARTITION_ALREADY_EXISTS(HttpStatus.CONFLICT, "ROOM-004", "이미 존재하는 파티션입니다."),
+    ROOM_HAS_NOT_PARTITION(HttpStatus.NOT_FOUND, "ROOM-005", "해당 룸에는 파티션이 존재하지 않습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String code;
--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/auth/service/OTPCodeService.java
+++
@@ -1,5 +0,0 @@
-package hufs.computer.studyroom.domain.auth.service;
-
-public interface OTPCodeService {
-    String generateRandomString(int length);
-}
