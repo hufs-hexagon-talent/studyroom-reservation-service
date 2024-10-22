87385f31edae5cd40da6919b44aba7794879d888
HwangBBang <h1009218@hufs.ac.kr>
2024-10-22 13:20:18 +0900

    [feat] Implement validation layer for partition domain

--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/partition/service/PartitionCommandService.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/partition/service/PartitionCommandService.java
@@ -1,6 +1,5 @@
 package hufs.computer.studyroom.domain.partition.service;
 
-import hufs.computer.studyroom.common.service.CommonHelperService;
 import hufs.computer.studyroom.domain.partition.dto.request.CreatePartitionRequest;
 import hufs.computer.studyroom.domain.partition.dto.request.ModifyPartitionRequest;
 import hufs.computer.studyroom.domain.partition.dto.response.PartitionInfoResponse;
@@ -8,10 +7,11 @@ import hufs.computer.studyroom.domain.partition.entity.RoomPartition;
 import hufs.computer.studyroom.domain.partition.mapper.RoomPartitionMapper;
 import hufs.computer.studyroom.domain.partition.repository.RoomPartitionRepository;
 import hufs.computer.studyroom.domain.room.entity.Room;
-import hufs.computer.studyroom.domain.schedule.repository.RoomOperationPolicyScheduleRepository;
+import hufs.computer.studyroom.domain.room.service.RoomQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
 
 @Slf4j
 @Service
@@ -19,33 +19,28 @@ import org.springframework.stereotype.Service;
 public class PartitionCommandService {
     private final RoomPartitionRepository partitionRepository;
     private final RoomPartitionMapper partitionMapper;
-    private final CommonHelperService commonHelperService;
+    private final PartitionQueryService partitionQueryService;
+    private final RoomQueryService roomQueryService;
 
     public PartitionInfoResponse createRoomPartition(CreatePartitionRequest request) {
-        Room room = commonHelperService.getRoomById(request.roomId());
+        Room room = roomQueryService.getRoomById(request.roomId());
         RoomPartition partition = partitionMapper.toRoomPartition(request, room);
         partitionRepository.save(partition);
         return partitionMapper.toInfoResponse(partition);
     }
 
     public PartitionInfoResponse modifyPartition(Long partitionId, ModifyPartitionRequest request) {
-        // 업데이트 할 파티션이 존재하는지?
-        RoomPartition partition = commonHelperService.getPartitionById(partitionId);
-        if (request.partitionNumber() != null) {
-            partition.setPartitionNumber(request.partitionNumber());
-        }
-        if (request.roomId() != null) {
-            Room room = commonHelperService.getRoomById(request.roomId());
-            partition.setRoom(room);
-        }
+
+        RoomPartition partition = partitionQueryService.getPartitionById(partitionId);
+        Room room = roomQueryService.getRoomById(request.roomId());
+
+        partitionMapper.updateFromRequest(request, room, partition);
 
         RoomPartition updatedPartition = partitionRepository.save(partition);
         return partitionMapper.toInfoResponse(updatedPartition);
     }
 
-    public Void deletePartitionById(Long partitionId) {
-        commonHelperService.getPartitionById(partitionId);// todo validatuon 애노테이션으로 컨트롤러단에서 처리
+    public void deletePartitionById(Long partitionId) {
         partitionRepository.deleteById(partitionId);
-        return null;
     }
 }
--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/partition/service/PartitionQueryService.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/partition/service/PartitionQueryService.java
@@ -1,7 +1,8 @@
 package hufs.computer.studyroom.domain.partition.service;
 
+import hufs.computer.studyroom.common.error.code.RoomErrorCode;
+import hufs.computer.studyroom.common.error.exception.CustomException;
 import hufs.computer.studyroom.common.error.exception.todo.reservation.RoomPolicyNotFoundException;
-import hufs.computer.studyroom.common.service.CommonHelperService;
 import hufs.computer.studyroom.domain.partition.dto.response.PartitionInfoResponse;
 import hufs.computer.studyroom.domain.partition.dto.response.PartitionInfoResponses;
 import hufs.computer.studyroom.domain.partition.dto.response.PartitionPolicyResponse;
@@ -13,6 +14,7 @@ import hufs.computer.studyroom.domain.policy.entity.RoomOperationPolicy;
 import hufs.computer.studyroom.domain.room.entity.Room;
 import hufs.computer.studyroom.domain.schedule.entity.RoomOperationPolicySchedule;
 import hufs.computer.studyroom.domain.schedule.repository.RoomOperationPolicyScheduleRepository;
+import hufs.computer.studyroom.domain.schedule.service.ScheduleQueryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -26,20 +28,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class PartitionQueryService {
     private final RoomPartitionRepository partitionRepository;
-    private final RoomOperationPolicyScheduleRepository scheduleRepository;
+    private final ScheduleQueryService scheduleQueryService;
     private final RoomPartitionMapper partitionMapper;
-    private final CommonHelperService commonHelperService;
 
     public PartitionInfoResponse findRoomPartitionById(Long roomPartitionId) {
-        RoomPartition partition = commonHelperService.getPartitionById(roomPartitionId);
+        RoomPartition partition = getPartitionById(roomPartitionId);
         return partitionMapper.toInfoResponse(partition);
-    }
-
-    public PartitionInfoResponses findByRoomId(Long roomId) {
-        commonHelperService.getRoomById(roomId);// todo validatuon 애노테이션으로 컨트롤러단에서 처리
-        List<RoomPartition> partitions = partitionRepository.findByRoom_RoomId(roomId);
-
-        return partitionMapper.toPartitionInfoResponses(partitions);
     }
 
     public PartitionInfoResponses findAll() {
@@ -50,21 +44,39 @@ public class PartitionQueryService {
     public PartitionPolicyResponses getPartitionsPolicyByDate(LocalDate date) {
 //        todo 수정
         List<RoomPartition> partitions = partitionRepository.findAll();
-        List<PartitionPolicyResponse> responseList = new ArrayList<>();
-        for (RoomPartition partition : partitions) {
-            RoomOperationPolicy policy = null;
-            Room room = partition.getRoom();
-            try {
-                RoomOperationPolicySchedule schedule = scheduleRepository.findByRoomAndPolicyApplicationDate(room, date).orElseThrow(() -> new RoomPolicyNotFoundException(room, date)// 운영이 하지 않음 (운영 정책 없음)
-                );
-                policy = schedule.getRoomOperationPolicy();
-            }catch (RoomPolicyNotFoundException e) {
-                // 정책이 없을 때는 policyId가 null로 유지됨
-            }
-            PartitionPolicyResponse response = partitionMapper.toPolicyResponse(partition, policy);
-            responseList.add(response);
-        }
 
+        List<PartitionPolicyResponse> responseList = partitions.stream()
+                .map(partition -> {
+                    Room room = partition.getRoom();
+                    RoomOperationPolicy policy = scheduleQueryService.getScheduleByRoomAndDate(room, date)
+                            .map(RoomOperationPolicySchedule::getRoomOperationPolicy)
+                            .orElse(null);
+                    return partitionMapper.toPolicyResponse(partition, policy); // Mapper로 변환
+                })
+                .toList();
         return partitionMapper.toPolicyResponses(responseList);
+    }
+
+//        List<PartitionPolicyResponse> responseList = new ArrayList<>();
+//        for (RoomPartition partition : partitions) {
+//            RoomOperationPolicy policy = null;
+//            Room room = partition.getRoom();
+//            try {
+//                RoomOperationPolicySchedule schedule = scheduleRepository.findByRoomAndPolicyApplicationDate(room, date).orElseThrow(() -> new RoomPolicyNotFoundException(room, date)// 운영이 하지 않음 (운영 정책 없음)
+//                );
+//                policy = schedule.getRoomOperationPolicy();
+//            }catch (RoomPolicyNotFoundException e) {
+//                // 정책이 없을 때는 policyId가 null로 유지됨
+//            }
+//            PartitionPolicyResponse response = partitionMapper.toPolicyResponse(partition, policy);
+//            responseList.add(response);
+//        }
+
+    public RoomPartition getPartitionById(Long id) {
+        return partitionRepository.findById(id).orElseThrow(() -> new CustomException(RoomErrorCode.PARTITION_NOT_FOUND));
+    }
+
+    public boolean existByPartitionId(Long id) {
+        return partitionRepository.existsById(id);
     }
 }
