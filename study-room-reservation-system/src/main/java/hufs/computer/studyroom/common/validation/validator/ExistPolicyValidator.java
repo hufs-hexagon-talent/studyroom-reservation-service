53d43d4e28ca7e050bc8874517a6eca2101c3325
HwangBBang <h1009218@hufs.ac.kr>
2024-10-22 13:23:50 +0900

    [feat] Add validation for checking existence of domain entities using custom validators

--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/common/validation/validator/ExistPolicyValidator.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/common/validation/validator/ExistPolicyValidator.java
@@ -1,6 +1,8 @@
 package hufs.computer.studyroom.common.validation.validator;
 
+import hufs.computer.studyroom.common.error.code.PolicyErrorCode;
 import hufs.computer.studyroom.common.validation.annotation.ExistPolicy;
+import hufs.computer.studyroom.domain.policy.service.PolicyQueryService;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
@@ -9,9 +11,16 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ExistPolicyValidator implements ConstraintValidator<ExistPolicy, Long> {
+    private final PolicyQueryService policyQueryService;
 
     @Override
-    public boolean isValid(Long aLong, ConstraintValidatorContext constraintValidatorContext) {
-        return false;
+    public boolean isValid(Long policyId, ConstraintValidatorContext context) {
+        boolean isValid = policyQueryService.existPolicyById(policyId);
+        if (!isValid) {
+            context.disableDefaultConstraintViolation(); // 기본 메시지 비활성화
+            context.buildConstraintViolationWithTemplate(PolicyErrorCode.POLICY_NOT_FOUND.toString()) // 커스텀 메시지 설정
+                    .addConstraintViolation(); // 커스텀 메시지 추가
+        }
+        return isValid;
     }
 }
--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/common/validation/validator/ExistScheduleValidator.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/common/validation/validator/ExistScheduleValidator.java
@@ -1,6 +1,8 @@
 package hufs.computer.studyroom.common.validation.validator;
 
+import hufs.computer.studyroom.common.error.code.ScheduleErrorCode;
 import hufs.computer.studyroom.common.validation.annotation.ExistSchedule;
+import hufs.computer.studyroom.domain.schedule.service.ScheduleQueryService;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
@@ -9,9 +11,16 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ExistScheduleValidator implements ConstraintValidator<ExistSchedule, Long> {
+    private final ScheduleQueryService scheduleQueryService;
 
     @Override
-    public boolean isValid(Long aLong, ConstraintValidatorContext constraintValidatorContext) {
-        return false;
+    public boolean isValid(Long scheduleId, ConstraintValidatorContext context) {
+        boolean isValid = scheduleQueryService.existByScheduleId(scheduleId);
+        if (!isValid) {
+            context.disableDefaultConstraintViolation(); // 기본 메시지 비활성화
+            context.buildConstraintViolationWithTemplate(ScheduleErrorCode.SCHEDULE_NOT_FOUND.toString()) // 커스텀 메시지 설정
+                    .addConstraintViolation(); // 커스텀 메시지 추가
+        }
+        return isValid;
     }
 }
