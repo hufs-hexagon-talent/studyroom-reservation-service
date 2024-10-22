f45b9a24f94cfb226fdcdcaf43cf22771e5c88fb
HwangBBang <h1009218@hufs.ac.kr>
2024-10-22 13:34:15 +0900

    [feat] Implement validation layer for schedule domain and Apply CQRS pattern to schedule domain

--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/department/controller/DepartmentController.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/department/controller/DepartmentController.java
@@ -2,30 +2,35 @@ package hufs.computer.studyroom.domain.department.controller;
 
 import hufs.computer.studyroom.common.response.SuccessResponse;
 import hufs.computer.studyroom.common.response.factory.ResponseFactory;
+import hufs.computer.studyroom.common.validation.annotation.ExistDepartment;
 import hufs.computer.studyroom.domain.department.dto.request.CreateDepartmentRequest;
 import hufs.computer.studyroom.domain.department.dto.request.ModifyDepartmentRequest;
 import hufs.computer.studyroom.domain.department.dto.response.DepartmentInfoResponse;
-import hufs.computer.studyroom.domain.department.service.DepartmentService;
+import hufs.computer.studyroom.domain.department.service.DepartmentCommandService;
+import hufs.computer.studyroom.domain.department.service.DepartmentQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Department", description = "관리 부서 정보 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/departments")
+@Validated
 public class DepartmentController {
-    private final DepartmentService departmentService;
+    private final DepartmentQueryService departmentQueryService;
+    private final DepartmentCommandService departmentCommandService;
 
     @Operation(summary = "✅[관리자] Department 생성",
             description = "Department 생성하는 API",
             security = {@SecurityRequirement(name = "JWT")})
     @PostMapping("/partition")
     public ResponseEntity<SuccessResponse<DepartmentInfoResponse>> createDepartment(@RequestBody CreateDepartmentRequest request) {
-        var result = departmentService.createDepartment(request);
+        var result = departmentCommandService.createDepartment(request);
         return ResponseFactory.created(result);
     }
 
@@ -33,8 +38,8 @@ public class DepartmentController {
             description = "Department 조회하는 API",
             security = {@SecurityRequirement(name = "JWT")})
     @PostMapping("/{departmentId}")
-    public ResponseEntity<SuccessResponse<DepartmentInfoResponse>> getDepartmentById(@PathVariable Long departmentId) {
-        var result = departmentService.findDepartmentById(departmentId);
+    public ResponseEntity<SuccessResponse<DepartmentInfoResponse>> getDepartmentById(@ExistDepartment @PathVariable Long departmentId) {
+        var result = departmentQueryService.findDepartmentById(departmentId);
         return ResponseFactory.success(result);
     }
 
@@ -43,9 +48,9 @@ public class DepartmentController {
             security = {@SecurityRequirement(name = "JWT")}
     )
     @PatchMapping("/{departmentId}")
-    public ResponseEntity<SuccessResponse<DepartmentInfoResponse>> updateDepartment(@PathVariable Long departmentId,
+    public ResponseEntity<SuccessResponse<DepartmentInfoResponse>> updateDepartment(@ExistDepartment @PathVariable Long departmentId,
                                                                                   @RequestBody ModifyDepartmentRequest request) {
-        var result = departmentService.updateDepartment(departmentId, request);
+        var result = departmentCommandService.updateDepartment(departmentId, request);
         return ResponseFactory.modified(result);
     }
     @Operation(summary = "✅[관리자] Department 삭제",
@@ -53,8 +58,9 @@ public class DepartmentController {
             security = {@SecurityRequirement(name = "JWT")}
     )
     @DeleteMapping("/{departmentId}")
-    public ResponseEntity<SuccessResponse<Void>> deleteDepartment(@PathVariable Long departmentId) {
-        departmentService.deleteDepartment(departmentId);
+    public ResponseEntity<SuccessResponse<Void>> deleteDepartment(
+            @ExistDepartment @PathVariable Long departmentId) {
+        departmentCommandService.deleteDepartment(departmentId);
         return ResponseFactory.deleted();
     }
 }
--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/room/service/RoomCommandService.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/room/service/RoomCommandService.java
@@ -1,7 +1,7 @@
 package hufs.computer.studyroom.domain.room.service;
 
 import hufs.computer.studyroom.domain.department.entity.Department;
-import hufs.computer.studyroom.domain.department.service.DepartmentService;
+import hufs.computer.studyroom.domain.department.service.DepartmentQueryService;
 import hufs.computer.studyroom.domain.room.dto.request.CreateRoomRequest;
 import hufs.computer.studyroom.domain.room.dto.request.ModifyRoomRequest;
 import hufs.computer.studyroom.domain.room.dto.response.RoomInfoResponse;
@@ -19,10 +19,10 @@ public class RoomCommandService {
     private final RoomRepository roomRepository;
     private final RoomMapper roomMapper;
     private final RoomQueryService roomQueryService;
-    private final DepartmentService departmentService;
+    private final DepartmentQueryService departmentQueryService;
 
     public RoomInfoResponse createRoom(CreateRoomRequest request) {
-        Department department = departmentService.getDepartmentById(request.departmentId());
+        Department department = departmentQueryService.getDepartmentById(request.departmentId());
         Room room = roomMapper.toRoom(request, department);
         roomRepository.save(room);
         return roomMapper.toInfoResponse(room);
@@ -30,7 +30,7 @@ public class RoomCommandService {
 
     public RoomInfoResponse updateRoom(Long roomId, ModifyRoomRequest request) {
         Room room = roomQueryService.getRoomById(roomId);
-        Department department = departmentService.getDepartmentById(request.departmentId());
+        Department department = departmentQueryService.getDepartmentById(request.departmentId());
         roomMapper.updateRoom(request, department, room);
 
         roomRepository.save(room);
