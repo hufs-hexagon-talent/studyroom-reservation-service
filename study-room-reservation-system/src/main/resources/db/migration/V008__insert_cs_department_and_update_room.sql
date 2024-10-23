5336410a107c937d9e49a504140b0fea4a53a717
HwangBBang <h1009218@hufs.ac.kr>
2024-10-23 18:59:23 +0900

    [fix] Add migration file

--- study-room-reservation-system/src/main/resources/db/migration/V008__insert_cs_department_and_update_room.sql
+++ study-room-reservation-system/src/main/resources/db/migration/V008__insert_cs_department_and_update_room.sql
@@ -5,8 +5,4 @@ INSERT INTO `service-db`.department (department_name) VALUES ('컴퓨터공학')
 UPDATE `service-db`.room
 SET department_id = 1
 WHERE room_id IN (1, 2);
-
--- 1-3. 기존 user 데이터 업데이트 (user_id가 10부터 441까지인 경우 1번 부서 할당)
-UPDATE `service-db`.user
-SET department_id = 1
-WHERE user_id BETWEEN 10 AND 441;
+-- User는 수동 업데이트
