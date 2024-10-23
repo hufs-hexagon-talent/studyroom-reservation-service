0d1da8f650110f3b6de84de0187077aca4ee81eb
HwangBBang <h1009218@hufs.ac.kr>
2024-10-23 18:42:39 +0900

    [refactor] Remove unused exception classes

--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/common/error/exception/todo/EmailCreationException.java
+++
@@ -1,9 +0,0 @@
-package hufs.computer.studyroom.common.error.exception.todo;
-
-import jakarta.mail.MessagingException;
-
-public class EmailCreationException extends RuntimeException {
-    public EmailCreationException(MessagingException e) {
-        super(e);
-    }
-}
--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/common/error/exception/todo/invalidvalue/InvalidValueException.java
+++
@@ -1,5 +0,0 @@
-package hufs.computer.studyroom.common.error.exception.todo.invalidvalue;
-
-public interface InvalidValueException {
-    String getMessage();
-}
