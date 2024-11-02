6cc6ea7b472cc0688e2d2579a27e1a490219e74f
HwangBBang <h1009218@hufs.ac.kr>
2024-11-03 00:29:40 +0900

    [fix] Correct bug in reservation status retrieval API for specific date and department

--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/partition/repository/RoomPartitionRepository.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/partition/repository/RoomPartitionRepository.java
@@ -2,6 +2,7 @@ package hufs.computer.studyroom.domain.partition.repository;
 
 import hufs.computer.studyroom.domain.partition.entity.RoomPartition;
 import hufs.computer.studyroom.domain.partition.entity.RoomPartition;
+import hufs.computer.studyroom.domain.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,6 +12,6 @@ public interface RoomPartitionRepository extends JpaRepository<RoomPartition,Lon
 //   RoomID로 PartitionId들 찾기
     List<RoomPartition> findByRoom_RoomId(Long roomId);
 
-    //   특정 Department에 속하는 모든 RoomPartition을 조회
-    List<RoomPartition> findAllByRoomDepartmentDepartmentId(Long departmentId);
+
+    List<RoomPartition> findByRoomIn(List<Room> rooms);
 }
