d3a6befb0ea0dd6bd31ab1dc0fc1ef938b43c09c
HwangBBang <h1009218@hufs.ac.kr>
2024-11-22 01:32:58 +0900

    [hotfix] : 정책 검증 관련 이슈 해결
    - DTO record 인터페이스 도입
    - validator 수정

--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/policy/dto/request/ModifyOperationPolicyRequest.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/policy/dto/request/ModifyOperationPolicyRequest.java
@@ -14,6 +14,4 @@ public record ModifyOperationPolicyRequest (
         @Schema(description = "[수정] 운영 시작 시간", example = "00:00:00") LocalTime operationStartTime,
         @Schema(description = "[수정] 운영 종료 시간", example = "10:00:00") LocalTime operationEndTime,
         @Positive @Schema(description = "[수정] 최대 예약 시간", example = "60") Integer eachMaxMinute
-){
-
-}
+) implements OperationPolicyRequest{}
