2827295b1985d03b3e9188a8328d1bb080e9ed91
HwangBBang <h1009218@hufs.ac.kr>
2025-03-05 11:34:06 +0900

    feat: 사용자 이메일 변경 기능 추가

---
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/user/dto/request/ModifyEmailRequest.java
@@ -0,0 +1,18 @@
+package hufs.computer.studyroom.domain.user.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+
+@Builder
+@Schema(description = "이메일 수정 요청 DTO")
+public record ModifyEmailRequest(
+        @Schema(description = "현재 비밀번호", example = "prePassword123")
+        @NotBlank(message = "현재 비밀번호를 입력해주세요.")
+        String prePassword,
+
+        @Schema(description = "새 이메일", example = "123456@hufs.ac.kr")
+        @NotBlank(message = "새 이메일을 입력해주세요.")
+        String newEmail
+) {
+}
