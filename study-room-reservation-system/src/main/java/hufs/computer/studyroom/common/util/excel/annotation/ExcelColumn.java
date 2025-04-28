1eb76aea8bb1e54d0b1a0569c7924a1609f29c74
HwangBBang <h1009218@hufs.ac.kr>
2025-04-28 15:35:32 +0900

    feat: implement annotations for Excel cell styling and column mapping

--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/common/util/excel/annotation/ExcelColumn.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/common/util/excel/annotation/ExcelColumn.java
@@ -1,44 +1,26 @@
 package hufs.computer.studyroom.common.util.excel.annotation;
 
 
+import hufs.computer.studyroom.common.util.excel.style.ExcelCellStyle;
+import hufs.computer.studyroom.common.util.excel.style.body.DefaultBodyStyle;
+import hufs.computer.studyroom.common.util.excel.style.header.DefaultHeaderStyle;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ * DTO 필드를 엑셀 컬럼에 매핑하기 위한 어노테이션
+ * - headerName: 컬럼 제목 지정
+ * - headerStyle: 헤더 셀 스타일 전략 지정
+ * - bodyStyle: 바디 셀 스타일 전략 지정
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ExcelColumn {
     String headerName() default "";
-//    ExcelColumnStyle headerStyle() default @ExcelColumnStyle(excelCellStyleClass = NoExcelCellStyle.class);
-//    ExcelColumnStyle bodyStyle() default @ExcelColumnStyle(excelCellStyleClass = NoExcelCellStyle.class);
-
+    Class<? extends ExcelCellStyle> headerStyle() default DefaultHeaderStyle.class;
+    Class<? extends ExcelCellStyle> bodyStyle()   default DefaultBodyStyle.class;
 }
-
-
-/*
-*
-  @ExcelColumn(
-        headerName = "회사",
-        headerStyle = @ExcelColumnStyle(excelCellStyleClass = GreyHeaderStyle.class),
-        bodyStyle = @ExcelColumnStyle(excelCellStyleClass = BodyStyle.class)
-  )
-  private final String company;
-
-
-  @ExcelColumn(
-        headerName = "차종",
-        headerStyle = @ExcelColumnStyle(excelCellStyleClass = GreyHeaderStyle.class),
-        bodyStyle = @ExcelColumnStyle(excelCellStyleClass = BodyStyle.class)
-  )
-  private final String name;
-*
-*
-*
-* | 회사 | 차종 |
-* -------------
-* |     |     |
-* |     |     |
-* |     |     |
-* |     |     |
-* */
--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/common/util/excel/annotation/ExcelColumnStyle.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/common/util/excel/annotation/ExcelColumnStyle.java
@@ -1,7 +1,12 @@
-//package hufs.computer.studyroom.common.util.excel.annotation;
-//
-//import hufs.computer.studyroom.common.util.excel.ExcelCellStyle;
-//
-//public @interface ExcelColumnStyle {
-//    Class<? extends ExcelCellStyle> excelCellStyleClass();
-//}
+package hufs.computer.studyroom.common.util.excel.annotation;
+
+
+import hufs.computer.studyroom.common.util.excel.style.ExcelCellStyle;
+
+/**
+ * 필드별 ExcelCellStyle 전략을 지정하는 어노테이션
+ * - value: 적용할 ExcelCellStyle 구현체 클래스
+ */
+public @interface ExcelColumnStyle {
+    Class<? extends ExcelCellStyle> value(); //excelCellStyleClass();
+}
