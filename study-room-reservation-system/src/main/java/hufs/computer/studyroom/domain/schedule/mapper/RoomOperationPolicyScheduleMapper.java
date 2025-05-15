681020b2b670a4fc4a5ee61594cc54a0677eb6bf
HwangBBang <h1009218@hufs.ac.kr>
2025-05-15 22:01:35 +0900

    fix: update response payload of GET /schedules/date/{policyApplicationDate}

--- study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/schedule/mapper/RoomOperationPolicyScheduleMapper.java
+++ study-room-reservation-system/src/main/java/hufs/computer/studyroom/domain/schedule/mapper/RoomOperationPolicyScheduleMapper.java
@@ -6,11 +6,13 @@ import hufs.computer.studyroom.domain.schedule.dto.request.ModifyScheduleRequest
 import hufs.computer.studyroom.domain.schedule.dto.response.AvailableDateResponses;
 import hufs.computer.studyroom.domain.schedule.dto.response.ScheduleInfoResponse;
 import hufs.computer.studyroom.domain.schedule.dto.response.ScheduleInfoResponses;
+import hufs.computer.studyroom.domain.schedule.dto.response.ScheduleTableResponse;
 import hufs.computer.studyroom.domain.schedule.entity.RoomOperationPolicySchedule;
 import org.mapstruct.*;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface RoomOperationPolicyScheduleMapper {
@@ -29,8 +31,25 @@ public interface RoomOperationPolicyScheduleMapper {
     @Mapping(target = "roomOperationPolicyScheduleId", ignore = true)
     void updateFromRequest(ModifyScheduleRequest request,Room room,RoomOperationPolicy roomOperationPolicy, @MappingTarget RoomOperationPolicySchedule schedule);
 
+    @Mapping(source = "roomOperationPolicyScheduleId", target = "scheduleId")
+    @Mapping(source = "room.roomId", target = "roomId")
+    @Mapping(source = "room.roomName", target = "roomName")
+    @Mapping(source = "roomOperationPolicy.roomOperationPolicyId", target = "policyId")
+    @Mapping(source = "roomOperationPolicy.operationStartTime", target = "operationStartTime")
+    @Mapping(source = "roomOperationPolicy.operationEndTime", target = "operationEndTime")
+    @Mapping(source = "roomOperationPolicy.eachMaxMinute", target = "eachMaxMinute")
+    @Mapping(source = "policyApplicationDate", target = "policyApplicationDate")
+    ScheduleTableResponse toScheduleOneLineResponse(RoomOperationPolicySchedule schedule);
+
     // 리스트 변환 (RoomOperationPolicySchedule 리스트 -> ScheduleInfoResponse 리스트)
     List<ScheduleInfoResponse> toScheduleInfoResponseList(List<RoomOperationPolicySchedule> schedules);
+
+    default List<ScheduleTableResponse> toScheduleTableResponse(List<RoomOperationPolicySchedule> schedules){
+        return schedules.stream()
+                .map(this::toScheduleOneLineResponse)
+                .collect(Collectors.toList());
+
+    }
 
     // 여러 RoomOperationPolicySchedule 리스트를 감싸는 응답 생성
     default ScheduleInfoResponses toScheduleInfoResponses(List<RoomOperationPolicySchedule> schedules) {
